2532f5eacbd5057bf42c354ee001269935f9d479
mirmubasheer <fahadk558.mmk@gmail.com>
2024-12-16 14:59:24 +0300

    second commit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  // Allow CORS for development and production environments
-  app.enableCors();
-  
+
+  // Enable CORS for development and production environments
+  app.enableCors({
+    origin: ['http://test-chatapp-b09g.onrender.com', 'http://localhost:3000'], // Adjust the origin as needed
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+
   // Use the port from the environment variable, default to 3000 for local development
   const port = process.env.PORT || 3000;
-  
-  await app.listen(port, () => {
+
+  // Listen on all interfaces for accessibility
+  await app.listen(port, '0.0.0.0', () => {
     console.log(`Application is running on: http://localhost:${port}`);
   });
 }
